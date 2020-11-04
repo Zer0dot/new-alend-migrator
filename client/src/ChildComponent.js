38bc0d27a0a18f8b1246fe2d607df72857cc7198
Zer0dot <zodiak90yt@gmail.com>
2020-11-04 11:23:16 -0500

    cleanup

--- client/src/ChildComponent.js
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import BlockchainContext from './BlockchainContext.js';
-
-export default function ChildComponent() {
-    const blockchainContext = useContext(BlockchainContext);
-
-    const { web3, contract, accounts } = blockchainContext;
-}
